95bf9adb20e5d0cd57e09a726134439ac84db6fa
MauriDemasi <mauridemasi88@gmail.com>
2024-07-20 07:36:23 -0300

    First Desktop Version

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function encriptar() {
+  validarInput();
+
   var texto = document.getElementById("text-input").value;
   var textoEncriptado = texto
     .replace(/e/gi, "enter")
@@ -8,14 +10,13 @@ function encriptar() {
     .replace(/u/gi, "ufat");
 
   var output = document.getElementsByClassName("output-section")[0];
-  output.innerHTML='';
-  console.log(output);
+  output.innerHTML = "";
 
   var parrafo = document.createElement("p");
   parrafo.classList.add("output-response");
   parrafo.id = "parrafo";
   parrafo.innerHTML = textoEncriptado;
-  
+
   output.appendChild(parrafo);
   output.classList.add("output-response");
 
@@ -24,6 +25,7 @@ function encriptar() {
 
 function desencriptar() {
   var texto = document.getElementById("text-input").value;
+  validarInput();
   var textoDesencriptado = texto
     .replace(/enter/gi, "e")
     .replace(/imes/gi, "i")
@@ -32,11 +34,17 @@ function desencriptar() {
     .replace(/ufat/gi, "u");
 
   var output = document.getElementsByClassName("output-section")[0];
-  output.innerText = textoDesencriptado;
+  output.innerHTML = "";
+
+  var parrafo = document.createElement("p");
+  parrafo.classList.add("output-response");
+  parrafo.id = "parrafo";
+  parrafo.innerHTML = textoDesencriptado;
+
+  output.appendChild(parrafo);
   output.classList.add("output-response");
 
   crearBotonCopiar();
-
 }
 
 function crearBotonCopiar() {
@@ -44,28 +52,37 @@ function crearBotonCopiar() {
 
   var botonCopiar = document.createElement("button");
   botonCopiar.classList.add("secondary");
+  botonCopiar.id = "btnCopiar";
 
   var spanButton = document.createElement("span");
   spanButton.classList.add("text-button");
-
 
   spanButton.innerText = "Copiar";
   botonCopiar.appendChild(spanButton);
 
   output.appendChild(botonCopiar);
 
-  botonCopiar.onclick = copiar; 
+  botonCopiar.onclick = copiar;
 }
-
 
 function copiar() {
   var texto = document.getElementById("parrafo").textContent;
-  navigator.clipboard.writeText(texto)
+  navigator.clipboard
+    .writeText(texto)
     .then(() => {
-      alert('Texto copiado en el  portapapeles');
+      alert("Texto copiado en el  portapapeles");
     })
-    .catch(err => {
-      console.error('Error copying text: ', err);
+    .catch((err) => {
+      console.error("Error copying text: ", err);
     });
+}
 
+function validarInput() {
+  var input = document.getElementById("text-input");
+  var regex = /^[a-z\s]+$/;
+
+  if (!regex.test(input.value)) {
+    alert("Solo se permiten letras minúsculas y espacios en blanco.");
+    input.value = "";
+  }
 }
