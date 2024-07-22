60bfbc9fecdd04a9565e5441b105aaf6869b5894
MauriDemasi <mauridemasi88@gmail.com>
2024-07-22 11:51:07 -0300

    End Tablet Version, and validate keypress

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function encriptar() {
-  validarInput();
-
   var texto = document.getElementById("text-input").value;
   var textoEncriptado = texto
     .replace(/e/gi, "enter")
@@ -10,13 +8,14 @@ function encriptar() {
     .replace(/u/gi, "ufat");
 
   var output = document.getElementsByClassName("output-section")[0];
-  output.innerHTML = "";
+  output.innerHTML='';
+  console.log(output);
 
   var parrafo = document.createElement("p");
   parrafo.classList.add("output-response");
   parrafo.id = "parrafo";
   parrafo.innerHTML = textoEncriptado;
-
+  
   output.appendChild(parrafo);
   output.classList.add("output-response");
 
@@ -25,7 +24,6 @@ function encriptar() {
 
 function desencriptar() {
   var texto = document.getElementById("text-input").value;
-  validarInput();
   var textoDesencriptado = texto
     .replace(/enter/gi, "e")
     .replace(/imes/gi, "i")
@@ -33,56 +31,78 @@ function desencriptar() {
     .replace(/ober/gi, "o")
     .replace(/ufat/gi, "u");
 
-  var output = document.getElementsByClassName("output-section")[0];
-  output.innerHTML = "";
+    var output = document.getElementsByClassName("output-section")[0];
+    output.innerHTML = ''; 
+    var parrafo = document.createElement("p");
+    parrafo.classList.add("output-response");
+    parrafo.id = "parrafo";
+    parrafo.innerHTML = textoDesencriptado;
+    
+    output.appendChild(parrafo);
+    output.classList.add("output-response");
+  
+    crearBotonCopiar();
 
-  var parrafo = document.createElement("p");
-  parrafo.classList.add("output-response");
-  parrafo.id = "parrafo";
-  parrafo.innerHTML = textoDesencriptado;
-
-  output.appendChild(parrafo);
-  output.classList.add("output-response");
-
-  crearBotonCopiar();
 }
 
+// Función para crear el botón de copiar
 function crearBotonCopiar() {
   var output = document.getElementsByClassName("output-section")[0];
 
-  var botonCopiar = document.createElement("button");
-  botonCopiar.classList.add("secondary");
-  botonCopiar.id = "btnCopiar";
+  var botonContenedor = document.createElement("div");
+  botonContenedor.classList.add("button");
 
-  var spanButton = document.createElement("span");
-  spanButton.classList.add("text-button");
+  var botonCopiar = document.createElement("div");
+  botonCopiar.classList.add("button1");
+  botonCopiar.innerText = "Copiar";
 
-  spanButton.innerText = "Copiar";
-  botonCopiar.appendChild(spanButton);
-
-  output.appendChild(botonCopiar);
-
-  botonCopiar.onclick = copiar;
+  botonContenedor.appendChild(botonCopiar);
+  output.appendChild(botonContenedor);
+  botonContenedor.onclick = copiar;
 }
 
+
+
+// Función para copiar texto al portapapeles
 function copiar() {
   var texto = document.getElementById("parrafo").textContent;
-  navigator.clipboard
-    .writeText(texto)
-    .then(() => {
-      alert("Texto copiado en el  portapapeles");
-    })
-    .catch((err) => {
-      console.error("Error copying text: ", err);
-    });
-}
 
-function validarInput() {
-  var input = document.getElementById("text-input");
-  var regex = /^[a-z\s]+$/;
-
-  if (!regex.test(input.value)) {
-    alert("Solo se permiten letras minúsculas y espacios en blanco.");
-    input.value = "";
+  // Verificar si navigator.clipboard está disponible
+  if (navigator.clipboard) {
+    navigator.clipb1oard.writeText(texto)
+      .then(() => {
+        alert('Texto copiado en el portapapeles');
+      })
+      .catch(err => {
+        console.error('Error copying text: ', err);
+      });
+  } else {
+    // Método alternativo para copiar el texto
+    var textArea = document.createElement("textarea");
+    textArea.value = texto;
+    document.body.appendChild(textArea);
+    textArea.select();
+    try {
+      document.execCommand('copy');
+      alert('Texto copiado en el portapapeles');
+    } catch (err) {
+      console.error('Error copying text: ', err);
+    }
+    document.body.removeChild(textArea);
   }
 }
+
+// funcion para escuchar cada tecla que se presiona y validar que sean solo letras y en minuscula
+function validarTexto(event) {
+  var charCode = event.charCode;
+  if (charCode >= 97 && charCode <= 122 || charCode == 32) {
+    return false;    
+  } else {
+    alert("Solo se permiten letras minúsculas y sin acentos");
+    event.preventDefault();
+    return true;
+  }
+}
+
+//aqui activamos la escucha de la tecla presionada
+document.getElementById("text-input").addEventListener("keypress", validarTexto);
